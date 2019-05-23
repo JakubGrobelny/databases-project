d562f854d38786306a6dd83de2fc93c19c25406c
Jakub Grobelny <jgrobelny1@gmail.com>
2019-05-23 21:17:10 +0200

    Working connection example

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,16 @@ module Main where
 
 import Database.PostgreSQL.Simple
 
+connectionInfo :: IO (Connection)
+connectionInfo = connect defaultConnectInfo 
+    {
+      connectDatabase = "testdb"
+    , connectUser     = "admin"
+    }
+
 main :: IO Int
 main = do
-    conn <- connectPostgreSQL ""
+    conn <- connectionInfo
     [Only i] <- query_ conn "select 2 + 2"
+    putStrLn $ show i
     return i
