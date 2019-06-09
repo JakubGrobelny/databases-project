82defc883c32dc5c9397aff37d11fab0fcf682e2
Jakub Grobelny <kubissus@gmail.com>
2019-06-09 19:40:53 +0200

    JSON parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,11 +4,11 @@ module Parser where
 
 import Data.Aeson
 
-data DatabaseInfo 
-    = DatabaseInfo { db       :: String
-                   , login    :: String
-                   , dbPasswd :: String
-                   } deriving Show
+data DatabaseInfo = DatabaseInfo 
+    { db       :: String
+    , login    :: String
+    , dbPasswd :: String
+    } deriving Show
 
 instance FromJSON DatabaseInfo where
     parseJSON = withObject "DatabaseInfo" $ \o -> do
@@ -17,11 +17,11 @@ instance FromJSON DatabaseInfo where
         dbPasswd <- o .: "password"
         return DatabaseInfo{..}
 
-data UserData
-    = UserData { time   :: Integer
-               , passwd :: String
-               , member :: Integer 
-               } deriving Show
+data UserData = UserData 
+    { time   :: Integer
+    , passwd :: String
+    , member :: Integer 
+    } deriving Show
 
 instance FromJSON UserData where
     parseJSON = withObject "UserData" $ \o -> do
@@ -30,11 +30,11 @@ instance FromJSON UserData where
         member <- o .: "member"
         return UserData{..}
 
-data ActionData
-    = ActionData { action    :: Integer
-                 , project   :: Integer
-                 , authority :: Maybe Integer
-                 } deriving Show
+data ActionData = ActionData 
+    { action    :: Integer
+    , project   :: Integer
+    , authority :: Maybe Integer
+    } deriving Show
 
 instance FromJSON ActionData where
     parseJSON = withObject "ActionData" $ \o -> do
@@ -43,10 +43,10 @@ instance FromJSON ActionData where
         authority <- o .:? "authority"
         return ActionData{..}
 
-data NewAction
-    = NewAction { newActionUser :: UserData
-                , newActionData :: ActionData
-                } deriving Show
+data NewAction = NewAction 
+    { newActionUser :: UserData
+    , newActionData :: ActionData
+    } deriving Show
 
 instance FromJSON NewAction where
     parseJSON = withObject "Action" $ \o -> do
@@ -54,10 +54,10 @@ instance FromJSON NewAction where
         newActionData <- parseJSON $ Object o
         return NewAction{..}
 
-data NewVote
-    = NewVote { newVoteUser :: UserData
-              , voteAction  :: Integer 
-              } deriving Show
+data NewVote = NewVote 
+    { newVoteUser :: UserData
+    , voteAction  :: Integer 
+    } deriving Show
 
 instance FromJSON NewVote where
     parseJSON = withObject "Vote" $ \o -> do
@@ -65,10 +65,10 @@ instance FromJSON NewVote where
         voteAction  <- o .: "action"
         return NewVote{..}
 
-data Projects
-    = Projects' { projectsUser      :: UserData
-                , projectsAuthority :: Maybe Integer
-                } deriving Show
+data Projects = Projects' 
+    { projectsUser      :: UserData
+    , projectsAuthority :: Maybe Integer
+    } deriving Show
 
 instance FromJSON Projects where
     parseJSON = withObject "Projects" $ \o -> do
@@ -81,11 +81,11 @@ data ActionsFilter
     | ActionsAuthority Integer
     deriving Show
 
-data Actions
-    = Actions' { actionsUser   :: UserData
-               , actionType    :: Maybe String
-               , actionsFilter :: Maybe ActionsFilter
-               } deriving Show
+data Actions = Actions' 
+    { actionsUser   :: UserData
+    , actionType    :: Maybe String
+    , actionsFilter :: Maybe ActionsFilter
+    } deriving Show
 
 instance FromJSON Actions where
     parseJSON = withObject "Actions" $ \o -> do
@@ -106,10 +106,10 @@ data VotesFilter
     | VotesProject Integer
     deriving Show
 
-data Votes
-    = Votes' { votesUser :: UserData
-             , votesFilter :: Maybe VotesFilter 
-             } deriving Show
+data Votes = Votes' 
+    { votesUser :: UserData
+    , votesFilter :: Maybe VotesFilter 
+    } deriving Show
 
 instance FromJSON Votes where
     parseJSON = withObject "Votes" $ \o -> do
@@ -124,14 +124,39 @@ instance FromJSON Votes where
                 let filter = Just $ VotesAction a
                 return $ Votes' votesUser filter
 
-data TrollsTimestamp
-    = TrollsTimestamp { timestamp :: Integer }
-    deriving Show
+data TrollsTimestamp = TrollsTimestamp { timestamp :: Integer } deriving Show
 
 instance FromJSON TrollsTimestamp where
     parseJSON = withObject "Trolls" $ \o -> do
         timestamp <- o .: "timestamp"
         return TrollsTimestamp{..}
+
+data APIFunctionJSON = APIFunctionJSON
+    { trolls   :: Maybe TrollsTimestamp
+    , open     :: Maybe DatabaseInfo
+    , leader   :: Maybe UserData
+    , protest  :: Maybe NewAction
+    , support  :: Maybe NewAction
+    , upvote   :: Maybe NewVote
+    , downvote :: Maybe NewVote
+    , actions  :: Maybe Actions
+    , projects :: Maybe Projects
+    , votes    :: Maybe Votes
+    } deriving Show
+
+instance FromJSON APIFunctionJSON where
+    parseJSON = withObject "Function" $ \o -> do
+        trolls   <- o .:? "trolls"
+        open     <- o .:? "open"
+        leader   <- o .:? "leader"
+        protest  <- o .:? "protest"
+        support  <- o .:? "support"
+        upvote   <- o .:? "upvote"
+        downvote <- o .:? "downvote"
+        actions  <- o .:? "actions"
+        projects <- o .:? "projects"
+        votes    <- o .:? "votes"
+        return APIFunctionJSON{..}
 
 data APIFunction
     = Trolls TrollsTimestamp
@@ -144,8 +169,24 @@ data APIFunction
     | Actions Actions
     | Projects Projects
     | Votes Votes
-    | None
+    deriving Show
 
 instance FromJSON APIFunction where
-    parseJSON = undefined
-    -- TODO:
+    parseJSON = withObject "Function" $ \o -> do
+        functions <- parseJSON $ Object o
+        case extractAPIFunction functions of
+            Nothing -> error "Invalid JSON input!"
+            Just f  -> return f
+
+extractAPIFunction :: APIFunctionJSON -> Maybe APIFunction
+extractAPIFunction APIFunctionJSON {trolls = Just t} = Just $ Trolls t
+extractAPIFunction APIFunctionJSON {open = Just o} = Just $ Open o
+extractAPIFunction APIFunctionJSON {leader = Just l} = Just $ Leader l
+extractAPIFunction APIFunctionJSON {protest = Just p} = Just $ Protest p
+extractAPIFunction APIFunctionJSON {support = Just s} = Just $ Support s
+extractAPIFunction APIFunctionJSON {upvote = Just u} = Just $ Upvote u
+extractAPIFunction APIFunctionJSON {downvote = Just d} = Just $ Downvote d
+extractAPIFunction APIFunctionJSON {actions = Just a} = Just $ Actions a
+extractAPIFunction APIFunctionJSON {projects = Just p} = Just $ Projects p
+extractAPIFunction APIFunctionJSON {votes = Just v} = Just $ Votes v
+extractAPIFunction _ = Nothing
