b449898d07a90959eb3c6035c392b2220e9965ab
Jakub Grobelny <jgrobelny1@gmail.com>
2019-06-10 17:10:44 +0200

    Result toJSON

--- src/Functions.hs
+++ src/Functions.hs
@@ -3,19 +3,48 @@
 module Functions where
 
 import Database.PostgreSQL.Simple
-import qualified Data.Aeson as Aeson
-
+import Data.Aeson
+import Data.Text
 import Parser
 
-data Value
-    = Number Integer
-    | Boolean Bool
+data ResultValue
+    = ResultNum Integer
+    | ResultBool Bool
+    deriving Show
 
-data Tuple = Tuple [Value]
+data Tuple = Tuple [ResultValue] deriving Show
 
 data Data 
     = Data [Tuple]
     | NoData
+    deriving Show
+
+data FunctionResult
+    = ResultError
+    | ResultOk [Tuple]
+    | ResultEmptyOK
+    deriving Show
+
+resultFromMaybe :: Maybe Data -> FunctionResult
+resultFromMaybe Nothing = ResultError
+resultFromMaybe (Just NoData) = ResultEmptyOK
+resultFromMaybe (Just (Data d)) = ResultOk d
+
+instance ToJSON ResultValue where
+    toJSON (ResultNum i) = toJSON i
+    toJSON (ResultBool b) = toJSON b
+
+instance ToJSON Tuple where
+    toJSON (Tuple vals) = toJSON vals
+
+instance ToJSON FunctionResult where
+    toJSON ResultError = object [
+        "status" .= ("ERROR" :: Text) ]    
+    toJSON ResultEmptyOK = object [
+        "status" .= ("OK" :: Text) ]    
+    toJSON (ResultOk d) = object [
+        "status" .= ("OK" :: Text),
+        "data" .= toJSON d ]
 
 executeFunction :: Connection -> APIFunction -> IO (Maybe Data)
 executeFunction _ _ = undefined
